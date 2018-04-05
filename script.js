ae1cc74b81db732b12c92c583a85455d7b88aff6
jlhelwig <jeff.helwig@gmail.com>
2018-04-05 15:10:07 -0500

    lowest number in an array, variation

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
+// function findSmallestInt(args) {
+//   let smallest=args[0]
+//   for (i=0; i<args.length-1; i++){
+//     if (smallest>args[i+1]){
+//       smallest = args[i+1];
+//     }
+//   }
+//   return smallest
+// }
+
+
 function findSmallestInt(args) {
-  let smallest=args[0]
-  for (i=0; i<args.length-1; i++){
-    if (smallest>args[i+1]){
-      smallest = args[i+1];
-    }
+    return console.log(Math.min(...args));
   }
-  return smallest
-}
 
 
 
